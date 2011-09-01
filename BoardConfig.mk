b43b7f3e383b19cc84c00fbd7fcd0550a4f62850
PlayfulGod <playfulgod@gmail.com>
2011-08-31 22:51:11 -0400

    booting again

---
+++ BoardConfig.mk
@@ -0,0 +1,103 @@
+#
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+USE_CAMERA_STUB := false
+BOARD_USE_FROYO_LIBCAMERA := true
+
+# inherit from the proprietary version
+-include vendor/huawei/ascend/BoardConfigVendor.mk
+
+PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=device/huawei/ascend/include/prelink-linux-arm.map
+TARGET_BOARD_PLATFORM := msm7k
+TARGET_CPU_ABI := armeabi-v6j
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv6j
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_KERNEL := false
+TARGET_NO_RECOVERY := false
+TARGET_NO_RADIOIMAGE := true
+
+# Recovery
+TARGET_PREBUILT_RECOVERY_KERNEL := device/huawei/ascend/recovery_kernel
+
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+#WPA_SUPPLICANT_VERSION := VER_0_6_X
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcm4319.ko"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/firmware/fw_bcm4319.bin nvram_path=/system/etc/firmware/nvram.txt"
+WIFI_DRIVER_MODULE_NAME     := "dhd"
+
+BOARD_KERNEL_CMDLINE := mem=211M console=ttyMSM2,115200n8 androidboot.hardware=qcom console=ttyUSBCONSOLE0 androidboot.console=ttyUSBCONSOLE0
+BOARD_KERNEL_BASE := 0x00200000
+BOARD_PAGE_SIZE := 4096
+BOARD_KERNEL_PAGESIZE := 4096
+
+# Graphics
+TARGET_HARDWARE_3D := false
+TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
+BOARD_NO_RGBX_8888 := true
+BOARD_AVOID_DRAW_TEXTURE_EXTENSION := true
+BOARD_HAS_LIMITED_EGL := true
+
+# Audio
+TARGET_PROVIDES_LIBAUDIO := true
+BOARD_USES_GENERIC_AUDIO := false
+
+# HW
+TARGET_USES_OLD_LIBSENSORS_HAL := true
+BOARD_HAVE_BLUETOOTH := true
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QCOM_LIBS := true
+BOARD_USES_QCOM_LIBRPC := true
+BOARD_VENDOR_USE_AKMD := akm8973
+
+# GPS
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := M860
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 6225
+
+# USB
+BOARD_USE_USB_MASS_STORAGE_SWITCH := true
+BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
+
+# MISC
+WITH_JIT := true
+ENABLE_JSC_JIT := true
+JS_ENGINE := v8
+
+# dev:    size   erasesize  name
+# mtd0: 00500000 00020000 "boot"
+# mtd1: 00500000 00020000 "recovery"
+# mtd2: 00140000 00020000 "misc"
+# mtd3: 00060000 00020000 "splash"
+# mtd4: 0aa00000 00020000 "system"
+# mtd5: 04600000 00020000 "cache"
+# mtd6: 0bda0000 00020000 "userdata"
+# mtd7: 01400000 00020000 "userdata2"
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00500000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0aa00000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0bda0000
+BOARD_FLASH_BLOCK_SIZE := 262144
+
+TARGET_OTA_ASSERT_DEVICE := ascend,M860
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_BOOTLOADER_BOARD_NAME=M860
+TARGET_BOOTLOADER_BOARD_NAME := M860
+
+TARGET_PREBUILT_KERNEL := device/huawei/ascend/kernel
